461974ecdc4aaf05ddfa4e295e1a417a937403c1
Zakariae Bakkari <zakariabakkari2006@gmail.com>
2025-11-03 09:34:30 +0100

    image stocker dans un tableau de deux dementions

---
+++ td2/image.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+void image(unsigned char tabImage[255][255], short int resultas[256]) {
+
+   //initialiser le tableau resultas a zero
+   for (int i = 0; i < 256; i++) {
+      resultas[i] = 0;
+   }
+   int ligne, colonne, valeurPixel;
+   for (ligne = 0; ligne < 255; ligne++) {
+      for (colonne = 0; colonne < 255; colonne++) {
+         valeurPixel = (unsigned char)tabImage[ligne][colonne];
+         resultas[valeurPixel]++;
+      }
+   }
+}
+
+int main(){
+   unsigned char tabImage[255][255];
+   short int resultas[256];
+
+   // Remplir le tableau tabImage avec des valeurs d'exemple
+   for (int i = 0; i < 255; i++) {
+      for (int j = 0; j < 255; j++) {
+         tabImage[i][j] = (char)(rand() % 256); // Exemple de remplissage
+      }
+   }
+
+   image(tabImage, resultas);
+
+   // Afficher les resultats
+   for (int i = 0; i < 256; i++) {
+      if (resultas[i] > 0) {
+         printf("Valeur du pixel %d: %d occurrences\n", i, resultas[i]);
+      }
+   }
+
+   return 0;
+}
